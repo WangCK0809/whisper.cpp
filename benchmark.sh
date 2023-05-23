d7bb627503e6b197cdd31f112aabcc42ebe3034c
joey.wang <joey.wang@cloudminds.com>
2023-05-23 15:12:58 +0800

    Modify benchmark.sh: output txt format.

--- benchmark.sh
+++ benchmark.sh
@@ -13,13 +13,12 @@ for dataset in $datasets; do
     # 清空输出文件
     > "$output_file"
     wav_dir=$data_root/$dataset/wav
-    # 遍历ASR_auto_test目录下的所有wav文件
     for wav_file in $wav_dir/*.wav; do
         # 提取文件名（不包含路径和扩展名）
         filename=$(basename "$wav_file" .wav)
         
         # 执行解码命令并将输出追加到输出文件
-        ./main -osrt -m "$model" -f "$wav_file" -l "$language" >> "$output_file"
+        ./main -otxt -m "$model" -f "$wav_file" -l "$language" >> "$output_file"
         
         # 在输出文件中插入一个空行作为音频之间的分隔符
         echo "" >> "$output_file"
